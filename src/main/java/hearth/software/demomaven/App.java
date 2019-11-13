996b074c5bee601428b9d57f5787a99e46b6220d
nightstone <julien.lenglet.pro@gmail.com>
2019-11-13 11:56:31 +0100

    Init

---
+++ src/main/java/hearth/software/demomaven/App.java
@@ -0,0 +1,13 @@
+package hearth.software.demomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
